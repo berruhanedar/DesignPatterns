a64af8dfeee67581147b37ae0bbca4c92607ee5b
Berru Hanedar <berruhanedar@icloud.com>
2026-01-31 23:56:30 +0300

    Singleton Design Pattern - Thread Safe

---
+++ src/main/java/com/berruhanedar/Creational/Singleton/Example2_MultiThreading_LazyLoading/Singleton.java
@@ -0,0 +1,25 @@
+package com.berruhanedar.Creational.Singleton.Example2_MultiThreading_LazyLoading;
+
+public class Singleton {
+
+    private static Singleton singleton;// Global Değişken
+
+    private static int number = 0;
+
+    private Singleton() {
+        System.out.println("Singleton Constructor");
+    }
+
+    public static Singleton getInstance() { // Thread Sfe
+        if (singleton == null) {
+            synchronized (Singleton.class) {
+                if (singleton == null) {
+                    singleton = new Singleton();
+                }
+            }
+        }
+        number++;
+        System.out.println(number);
+        return singleton;
+    }
+}
